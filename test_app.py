0c07553942d7b11d71ed0fa984a82e089904c717
Nalin Kularathna <pgnaleen@gmail.com>
2021-01-09 15:36:10 +0000

    modifying test

--- test_app.py
+++ test_app.py
@@ -1,5 +1,8 @@
 import unittest
+import requests
+import json
 from app import hello
+from unittest.mock import patch
 
 class TestHelloApp(unittest.TestCase):
 
@@ -15,8 +18,14 @@ class TestHelloApp(unittest.TestCase):
 #      self.mysql.start()
 #      self.db_conn.execute("""CREATE TABLE `foo` (blah)""")
 
-  def test_hello(self):
-    self.assertEqual(hello(), '{"status": "success"}')
+#  def test_hello(self):
+#    self.assertEqual(hello(), '{"status": "success"}')
+
+  @patch('requests.post')
+  def test_post(self, mock_post):
+      info = {"user_id": "value1", "items": "value2", "total_price": 5400}
+      resp = requests.post("localhost:80", data=json.dumps(info), headers={'Content-Type': 'application/json'})
+      mock_post.assert_called_with("localhost:80", data=json.dumps(info), headers={'Content-Type': 'application/json'})
 
 if __name__ == '__main__':
   unittest.main()
